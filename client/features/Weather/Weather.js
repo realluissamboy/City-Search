71a9e304015f901dfb4937da8c340c48d4af47e1
Luis Samboy <luissamboy@icloud.com>
2023-04-30 00:16:31 -0700

    Created temperature conversion function

--- client/features/Weather/Weather.js
+++ client/features/Weather/Weather.js
@@ -11,13 +11,18 @@ const Weather = () => {
     dispatch(fetchWeather())
   }, [dispatch])
 
+  const convertToFahrenheit = (kelvin) => {
+    const fahrenheit = (kelvin - 273.15) * 1.8 + 32
+    return fahrenheit.toFixed(1)
+  }
+
   return (
     <div>
       {weather.main && (
         <div>
           <h3>{weather.name}</h3>
-          <p>Temperature: {weather.main.temp}°C</p>
-          <p>Feels like: {weather.main.feels_like}°C</p>
+          <p>Temperature: {convertToFahrenheit(weather.main.temp)}°F</p>
+          <p>Feels like: {convertToFahrenheit(weather.main.feels_like)}°F</p>
           <p>Humidity: {weather.main.humidity}%</p>
           <p>Weather: {weather.weather[0].main}</p>
         </div>
