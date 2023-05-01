9fbb845414c7802eb820130380b4c3ecb6c7e7e0
Luis Samboy <luissamboy@icloud.com>
2023-05-01 10:44:25 -0700

    Finally cracked the code. ChapGPT suggestions are appearing on the front end

--- client/features/Generate/Generate.js
+++ client/features/Generate/Generate.js
@@ -1,47 +1,35 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 const Generate = ({ city }) => {
-  const [cityInput, setCityInput] = useState('')
   const [result, setResult] = useState(null)
 
-  async function onSubmit(event) {
-    event.preventDefault()
-
+  async function generateSuggestions(city) {
     try {
       const response = await fetch('/api/generate', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ city: cityInput }),
+        body: JSON.stringify({ city }),
       })
 
       const data = await response.json()
       setResult(data)
-      setCityInput('')
     } catch (error) {
-      // Consider implementing your own error handling logic here
       console.error(error)
       alert(error.message)
     }
   }
 
+  useEffect(() => {
+    generateSuggestions(city)
+  }, [city])
+
   return (
     <div>
-      <h3>Suggest me 3 things to do</h3>
-      <form onSubmit={onSubmit}>
-        <input
-          type="text"
-          name="city"
-          placeholder="Enter a city"
-          value={cityInput}
-          onChange={(e) => setCityInput(e.target.value)}
-        />
-        <input type="submit" value="Generate suggestions" />
-      </form>
       {result && result.result && (
         <div>
-          <h4>Suggestions for {cityInput}:</h4>
+          <h2>Top 3 Places to visit in {city}:</h2>
           <ul>
             {result.result.split(', ').map((item, i) => (
               <li key={i}>{item}</li>
